555985caa5d33de70c8e46928edc4cebdca281c7
jbkr <baker.james.jb@gmail.com>
2017-03-02 13:01:20 +0800

    for testing on linux

--- complexity.py
+++ complexity.py
@@ -1,14 +1,14 @@
 import sys
+import subprocess
+from subprocess import call
 from pymol import cmd, stored
 
-
 def complexity(selection='all'):
-    # DESCRIPTION
     '''
     In pymol type `run Path/To/complexity_in_pymol/complexity.py` and hit enter. Then type `complexity` in the pymol terminal. This function will extract and recolour transmembrane helices according to the complexity score of the helix.
     '''
 
-    write_input_fasta_file = open("inputseq.fasta", "w")
+    write_input_fasta_file = open("sequence.fasta", "w")
     write_segment_positions_file = open("TMsegments.txt", "w")
 
     cmd.hide("all")
@@ -17,24 +17,27 @@ def complexity(selection='all'):
 
     # writing the fasta sequence to the input
     sequence = cmd.get_fastastr('all')
+    print(sequence)
     write_input_fasta_file.writelines([sequence])
 
     # Do sequence predition. For now, let's use the predefined ones for 1MT5,
-    # which is probably a SP and a TMH.
-    tmh_list = [[410, 438]]
+    tmh_list = [[5,25]]
 
     # It is much less prone to error if only 1 TMH is fed at a time.
     for a_tmh in tmh_list:
         start = int(a_tmh[0])
         stop = int(a_tmh[1])
-        print("Checking tmh at positions %s-%s" % (start, stop))
-        output_locations = str(str(start) + " , " + str(stop))
+        print(("Checking tmh at positions %s-%s" % (start, stop)))
+        output_locations = str(str(start) + "," + str(stop))
         write_segment_positions_file.writelines([output_locations])
 
-        # run tmsoc
+        #Let's set the TMH complexity to a null value incase of an error.
         tmh_complexity = "NONE"
-        commandstring = 'bash perl.sh'
-        os.system(commandstring)
+
+        # run tmsoc
+        commandstring = 'perl TMSOC.pl ./sequence.fasta ./TMsegments.txt >> TMSOCoutput.txt'
+        subprocess.call([commandstring], shell=True)
+
 
         # reading the TMSOC result
 
@@ -43,18 +46,23 @@ def complexity(selection='all'):
         tmsoc_result = open("TMSOCoutput.txt", 'r')
 
         for line in tmsoc_result:
+            print(line)
             if ";simple" in line:
-                complexity = "Simple"
+                tmh_complexity = "Simple"
             elif ";complex" in line:
-                complexity = "Complex"
+                tmh_complexity = "Complex"
             elif ";twilight" in line:
-                complexity = "Twilight"
+                tmh_complexity = "Twilight"
             else:
-                print(line)
                 pass
 
-        cmd.select("1MT5_B", "resi %s-%s" % (start, stop))
-        cmd.show('cartoon', "1MT5_B")
+        #Remove the output to stop it intefering with other iterations.
+        subprocess.call("rm sequence.fasta", shell=True)
+        subprocess.call("rm TMsegments.txt", shell=True)
+        subprocess.call("rm TMSOCoutput.txt", shell=True)
+
+
+        cmd.select("new_selection", "resi %s-%s" % (start, stop))
         if tmh_complexity == "Complex":
             cmd.color('red', "resi %s-%s" % (start, stop))
         elif tmh_complexity == "Simple":
@@ -62,8 +70,10 @@ def complexity(selection='all'):
         elif tmh_complexity == "Twilight":
             cmd.color('purple', "resi %s-%s" % (start, stop))
         elif tmh_complexity == "NONE":
+            cmd.color('orange', "resi %s-%s" % (start, stop))
             print("Complexity calculation failed.")
-        print("%s-%s complexity=" % (start, stop), tmh_complexity)
 
+        print(("%s-%s complexity=" % (start, stop), tmh_complexity))
+    print("Red is complex, blue is simple, and purple is twighlight. Orange helices indicate an error.")
 
 cmd.extend("complexity", complexity)
--- readme.md
+++ readme.md
@@ -3,5 +3,9 @@ This is a development project. The aim is to integrate a TMSOC like function int
 
 In pymol type `run Path/To/complexity_in_pymol/complexity.py` and hit enter. Then type `complexity` in the pymol terminal. This function will extract and recolour transmembrane helices according to the complexity score of the helix.
 
+#To do
+[ ] TM prediction integration.
+
+[x] TMSOC integration.
 
 [![Code Issues](https://www.quantifiedcode.com/api/v1/project/8a4ca942e31146de8448bb69a75c384f/badge.svg)](https://www.quantifiedcode.com/app/project/8a4ca942e31146de8448bb69a75c384f)
