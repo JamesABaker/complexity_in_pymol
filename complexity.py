76cb1807e7ca7925cd87800bd1b3bc8ffee5933b
jbkr <baker.james.jb@gmail.com>
2017-03-02 10:40:15 +0800

    fixed potential bug by avoiding wildcard import. Fixed minor issue of non-Pythonic variable names. Changed comments to docstring in complexity function.

--- complexity.py
+++ complexity.py
@@ -1,20 +1,17 @@
 import sys
-from Bio import SeqIO
-from Bio.PDB import *
 from pymol import cmd, stored
-
 
 
 def complexity(selection='all'):
     # DESCRIPTION
-    # In pymol type `run Path/To/complexity_in_pymol/complexity.py` and hit
-    # enter. Then type `complexity` in the pymol terminal. This function will
-    # extract and recolour transmembrane helices according to the complexity
-    # score of the helix.
+    '''
+    In pymol type `run Path/To/complexity_in_pymol/complexity.py` and hit enter. Then type `complexity` in the pymol terminal. This function will extract and recolour transmembrane helices according to the complexity score of the helix.
+    '''
 
     write_input_fasta_file = open("inputseq.fasta", "w")
     write_segment_positions_file = open("TMsegments.txt", "w")
 
+    cmd.hide("all")
     cmd.show('cartoon', "all")
     cmd.color('gray', "all")
 
@@ -24,20 +21,20 @@ def complexity(selection='all'):
 
     # Do sequence predition. For now, let's use the predefined ones for 1MT5,
     # which is probably a SP and a TMH.
-    tmh_list = [[410 , 438]]
+    tmh_list = [[410, 438]]
 
-    #It is much less prone to error if only 1 TMH is fed at a time.
+    # It is much less prone to error if only 1 TMH is fed at a time.
     for a_tmh in tmh_list:
-        start=int(a_tmh[0])
-        stop=int(a_tmh[1])
-        print("Checking tmh at positions %s-%s"%(start,stop))
+        start = int(a_tmh[0])
+        stop = int(a_tmh[1])
+        print("Checking tmh at positions %s-%s" % (start, stop))
         output_locations = str(str(start) + " , " + str(stop))
         write_segment_positions_file.writelines([output_locations])
 
         # run tmsoc
         tmh_complexity = "NONE"
-        commandString = 'bash perl.sh'
-        os.system(commandString)
+        commandstring = 'bash perl.sh'
+        os.system(commandstring)
 
         # reading the TMSOC result
 
@@ -56,19 +53,17 @@ def complexity(selection='all'):
                 print(line)
                 pass
 
-
-        cmd.select("1MT5_B", "resi %s-%s"%(start,stop))
+        cmd.select("1MT5_B", "resi %s-%s" % (start, stop))
         cmd.show('cartoon', "1MT5_B")
-        if tmh_complexity=="Complex":
-            cmd.color('red', "resi %s-%s"%(start,stop))
-        elif tmh_complexity=="Simple":
-            cmd.color('blue', "resi %s-%s"%(start,stop))
-        elif tmh_complexity=="Twilight":
-            cmd.color('purple', "resi %s-%s"%(start,stop))
-        elif tmh_complexity=="NONE":
+        if tmh_complexity == "Complex":
+            cmd.color('red', "resi %s-%s" % (start, stop))
+        elif tmh_complexity == "Simple":
+            cmd.color('blue', "resi %s-%s" % (start, stop))
+        elif tmh_complexity == "Twilight":
+            cmd.color('purple', "resi %s-%s" % (start, stop))
+        elif tmh_complexity == "NONE":
             print("Complexity calculation failed.")
-        print("%s-%s complexity="%(start,stop), tmh_complexity)
-
+        print("%s-%s complexity=" % (start, stop), tmh_complexity)
 
 
 cmd.extend("complexity", complexity)
