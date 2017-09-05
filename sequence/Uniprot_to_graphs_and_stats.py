a8df07843a9a046bb3d092a249088cb1c1f09845
jbkr <baker.james.jb@gmail.com>
2017-09-05 16:28:54 +0800

    Complexity function added

--- sequence/Uniprot_to_graphs_and_stats.py
+++ sequence/Uniprot_to_graphs_and_stats.py
@@ -48,7 +48,7 @@ def complexity_extraction(sequence, tmh_locations):
     '''
     Returns a list with values of complexities of TMHs.
     '''
-
+    list_of_complexities=[]
     # Generates the TMSOC input files from the source file.
     with open("TMsegments.txt", 'w') as tm_segments_file:
         tm_segments_file.write(tmh_locations)
@@ -58,133 +58,32 @@ def complexity_extraction(sequence, tmh_locations):
         fasta_file.write(str(sequence))
 
     # Runs TMSOC.
-    # The command we need to run is perl TMSOC.pl sequence.fasta TMsegments.txt > tmsoc_output.txt
-    perl_script=subprocess.check_output(["perl", "TMSOC.pl", "sequence.fasta", "TMsegments.txt"])
-    print(perl_script)
-    # Processes the TMSOC input files.
-    new_tmsoc_result = "1. TM segment(s) summary:"
-    all_results = []
-    current_result = []
+    perl_script_output=subprocess.check_output(["perl", "TMSOC.pl", "sequence.fasta", "TMsegments.txt"])
 
-    for line in open('tmsoc_output.txt'):
-        if line.startswith(new_tmsoc_result) and current_result:
-            # if line starts with thew new TMSOC result token and the current chunk
-            # is not empty
-            # add not empty chunk to chunks
-            all_results.append(current_result[:])
-            current_result = []  # make current chunk blank
-        # just append a line to the current chunk on each iteration
-        current_result.append(line)
+    # Processes the TMSOC input files. Normally the TMSOC output would be a multi-line file, not a string.
+    list_of_output_lines=str(perl_script_output.decode('UTF-8'))
+    list_of_output_lines=list_of_output_lines.replace("\n", ":")
+    list_of_output_lines=list_of_output_lines.split(":")
+    for line in list_of_output_lines:
 
-    # append the last chunk outside the loop
-    all_results.append(current_result)
-
-    for current_result in all_results:
-        b_factors = []
-        print("Processing TMSOC result chunk:", current_result)
-        for tmsoc_result_line in current_result:
-            if ">" in tmsoc_result_line:
-                tmsoc_result_line = tmsoc_result_line.split(";")
-                # Here we ignore the first entry, which is simply ">"
-                tmsoc_result_line_pdb_code = str(tmsoc_result_line[1])
-                tmsoc_result_line_chain_id = str(tmsoc_result_line[2])
-                tmsoc_result_line_start_location = int(tmsoc_result_line[3])
-                tmsoc_result_line_end_location = int(tmsoc_result_line[4])
-                tmsoc_result_line_breaks = list(tmsoc_result_line[5])
-                # masked fast doesn't always exist
-                # tmsoc_result_line_masked_sequence=str(tmsoc_result_line[6])
-
-        # Change this to proper length of the segment and add compensation factors for + AND - start positions
-        # for record in SeqIO.parse("sequence.fasta", "fasta"):
-        #    print(record.seq)
-        #    for n in range(len(record.seq)):
-        #        b_factors.append(0)
-
-        with open('sequence.fasta', "r") as sequence_file:
-            lines = sequence_file.read().splitlines()
-            #[] needs a ; at the end
-            records_from_lines = ''.join(lines).split('>;')
-            print("Looking for chunk match in FASTA file...")
-            for record in records_from_lines:
-                print("Checking chunk:", record)
-                record = record.split(";")
-                if len(record) == 6:
-                    record_pdb_code = str(record[0])
-                    record_chain_id = str(record[1])
-                    record_start_location = int(record[2])
-                    record_end_location = int(record[3])
-                    record_breaks = list(record[4])
-                    record_sequence = str(record[5])
-
-                    # Integrity checking.
-                    if len(record_sequence) == abs(record_start_location - record_end_location):
-                        print("WARNING! Length mismatch in",
-                              record_pdb_code, "chain ", record_chain_id)
-                    if abs(record_start_location - record_end_location) == len(record_breaks):
-                        print("Mismatch distance matches number of breaks")
-
-                    # Checks if the record matches the current TMSOC chunk ind
-                    # terms of PDB code and chain ID.
-                    if record_pdb_code == tmsoc_result_line_pdb_code and record_chain_id == tmsoc_result_line_chain_id:
-                        # Sets a new variable to hold the current result chunk if
-                        # it finds a match. This needs to be checked and reset.
-                        current_result_record = record
-                        current_result_record_chain_id = str(
-                            current_result_record[1])
-                        current_result_record_start_location = int(
-                            current_result_record[2])
-                        current_result_record_end_location = int(
-                            current_result_record[3])
-                        current_result_record_breaks = list(
-                            current_result_record[4])
-                        current_result_record_sequence = str(
-                            current_result_record[5])
-
-                        # Needs testing to accomodate premature/- start
-                        # positions
-                        for n in range(len(record_sequence) + record_start_location):
-                            b_factors.append(0)
-                else:
-                    print(
-                        "Record contained the wrong number of items, skipping to next record.")
-                    pass
-
-        # This is the list that will be printed as a graph.
-        complexity_scores_for_plot = []
-
-        for item_position, line in enumerate(current_result):
-            line = line.replace(',', ";")
-            line = line.split(';')
-
-            if str("1. TM segment(s) summary:") not in str(line) and str('2. Masked FASTA sequence:') not in str(line) and str('>') not in str(line) and len(line) == 7:
-                print(line)
-                tmh_sequence = line[0]
-                # These start and end positions are NOT the real start positions,
-                # but rather distances from the beining of the first amino acid in
-                # the fasta file sequence.
-                start_position = int(line[1])
-                end_position = int(line[2])
-                complexity_score = float(line[3])
-                hydrophobicity_score = float(line[4])
-                z_score = line[5]
-                complexity = line[6]
-                if start_position == -1 and end_position == -1:
-                    print("Skipping placeholder entry")
-                    pass
-                else:
-                    complexity_scores_for_plot.append(complexity_score)
-
-                    for residue_number, residue in enumerate(current_result_record_sequence):
-                        if residue_number <= end_position and residue_number >= start_position and end_position - start_position > 1:
-                            # need to add option for complexity interpretation
-                            b_factors[
-                                residue_number + current_result_record_start_location] = str(complexity_score)
-                            # b_factors[residue_number+current_result_record_start_location] = b_factor_complexity(str(complexity))
-                        else:
-                            pass
-
-
-
+        line = str(line.replace(',', ";"))
+        line = line.split(";")
+        #First we can ignore the "junk" lines
+        if len(line)==7:
+            tmh_sequence = line[0]
+            start_position = int(line[1])
+            end_position = int(line[2])
+            complexity_score = float(line[3])
+            hydrophobicity_score = float(line[4])
+            z_score = line[5]
+            complexity = line[6]
+            #Lenght restrictions
+            if start_position == -1 and end_position == -1:
+                print("Skipping placeholder entry")
+                pass
+            else:
+                list_of_complexities.append(complexity_score)
+    return(list_of_complexities)
 
 # Before we conduct the analysis we must determine how many empty TMH
 # lists to make. For example if the protein with the most TMHs in the
